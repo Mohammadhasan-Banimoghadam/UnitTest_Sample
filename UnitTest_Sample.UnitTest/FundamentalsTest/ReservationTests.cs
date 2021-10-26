9dd6984513c6b4f7fe4cd6f11c21aed93dc9aab8
Mohammadhasan banimoghadam <mhwmd.bm@gmail.com>
2021-10-26 23:15:44 +0330

    Reservation Class UnitTest

---
+++ UnitTest_Sample.UnitTest/FundamentalsTest/ReservationTests.cs
@@ -0,0 +1,37 @@
+using System;
+using UnitTest_Sample.Fundamentals;
+using Xunit;
+
+namespace UnitTest_Sample.UnitTest
+{
+    public class ReservationTests
+    {
+        [Fact]
+        public void CanBeCancelBy_AdminCancelling_ReturnsTrue()
+        {
+            //arrange
+            var reservation = new Reservation();
+            //act
+            var result = reservation.CanBeCancelBy(new User() { IsAdmin = true });
+            //assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanBeCancelBy_SameUserCancelling_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation() { MadeBy = user };
+            var result = reservation.CanBeCancelBy(user);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanBeCancelBy_OtherUserCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation() { MadeBy = new User() };
+            var result = reservation.CanBeCancelBy(new User());
+            Assert.False(result);
+        }
+    }
+}
---
+++ UnitTest_Sample/Fundamentals/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTest_Sample.Fundamentals
+{
+    public class Reservation
+    {
+        public User MadeBy;
+
+        public bool CanBeCancelBy(User user)
+        {
+            return (user.IsAdmin || MadeBy == user);
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
